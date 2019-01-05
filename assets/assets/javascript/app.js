77d17ae79c701f0fd6cedef24cdea83e578b22e1
MinhTran0220 <minhngocny@hotmail.com>
2019-01-05 14:19:45 -0500

    Finished

--- assets/assets/javascript/app.js
+++ assets/assets/javascript/app.js
@@ -27,9 +27,13 @@ $(document).ready(function () {
 
     //$("#animalbutton").empty();
 
+
+
     // create for looping for array of Animals 
 
     for (var i = 0; i < arrayAnimals.length; i++) {
+
+      // Then dynamicaly generating buttons for each movie in the array
 
       // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
 
@@ -50,6 +54,9 @@ $(document).ready(function () {
       // Adding the button to the HTML
 
       $(aninmalButton).append(a);
+
+
+
     }
 
   }
@@ -70,9 +77,12 @@ $(document).ready(function () {
 
     var animal = $(this).attr("data-animal");
 
+
+
     // Constructing a queryURL using the animal name + key for 10 limit
 
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+
 
 
     // Performing an AJAX request with the queryURL
@@ -80,6 +90,7 @@ $(document).ready(function () {
     $.ajax({
 
       url: queryURL,
+
       method: "GET"
 
     })
@@ -88,9 +99,13 @@ $(document).ready(function () {
 
       .then(function (response) {
 
+
+
         // Storing an array of results in the results variable
 
         var results = response.data;
+
+
 
         // Looping through each result item
 
@@ -100,19 +115,27 @@ $(document).ready(function () {
 
           var animalDiv = $("<div/>").addClass("animal-item");
 
+
+
           // Storing the result item's rating
 
           var rating = results[i].rating;
 
+
+
           // Creating a paragraph tag with the result item's rating
 
           var p = $("<p>").text("Rating: " + rating);
+
+
 
           // Setting the src attribute of the image to a property pulled off the result item
 
           var animateImg = results[i].images.fixed_height.url;
 
           var result = results[i].images.fixed_height_still.url;
+
+
 
           // Creating and storing an image tag
 
@@ -139,31 +162,34 @@ $(document).ready(function () {
           // Prependng the animalDiv to the HTML page in the "#animals" div
 
           $("#animals").append(animalDiv);
+
+
+
         }
 
       });
 
   });
 
-  // This function handles events where one button is clicked
-  $("#add-animal").on("click", function (event) {
+// This function handles events where one button is clicked
+$("#add-animal").on("click", function (event) {
 
-    // Preventing the buttons default behavior when clicked (which is submitting a form)
-    event.preventDefault();
+  // Preventing the buttons default behavior when clicked (which is submitting a form)
+  event.preventDefault();
 
-    // This line grabs the input from the textbox
-    var newAnimal = $("input").eq(0).val();
-    //var newAnimal = $("input").val().trim();
+  // This line grabs the input from the textbox
+  var newAnimal = $("input").eq(0).val();
+  //var newAnimal = $("input").val().trim();
 
 
-    if (newAnimal.length > 2) {
-      // Adding the animals from the textbox to our array
-      animals.push(newAnimal);
-    }
-    // Running the populateButtons function(passing in the animals as an argument)
-    allClicks(animals, "animal-button", "#animal-buttons");
+  if (newAnimal.length > 2) {
+    // Adding the animals from the textbox to our array
+    animals.push(newAnimal);
+  }
+  // Running the populateButtons function(passing in the animals as an argument)
+  allClicks(animals, "animal-button", "#animal-buttons");
 
-  });
+});
 
 
   // Function for displaying the animals info
@@ -179,6 +205,8 @@ $(document).ready(function () {
     // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
 
     var state = $(this).attr("data-state");
+
+
 
     // If the clicked image's state is still, update its src attribute to what its data-animate value is.
 
